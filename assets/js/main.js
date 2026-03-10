959c6571b1cbec7302d9c045849ba70a41a5c67b
hkkendi <kending@gumhk.com>
2026-03-10 12:25:33 +0800

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,7 +32,7 @@ const observer = new IntersectionObserver((entries) => {
   });
 }, { threshold: 0.1 });
 
-document.querySelectorAll('.project-card, .skill-group, .timeline-item').forEach(el => {
+document.querySelectorAll('.project-card, .project-card-compact, .skill-group, .timeline-item').forEach(el => {
   el.classList.add('fade-in');
   observer.observe(el);
 });
